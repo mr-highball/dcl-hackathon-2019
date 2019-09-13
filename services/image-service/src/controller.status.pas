ef31aad509033073c0ac56b62b09e368c2f73f15
mr-highball <35942565+mr-highball@users.noreply.github.com>
2019-09-12 23:20:52 -0400

    closes #7 - initializes status table inside of status controller

--- services/image-service/src/controller.status.pas
+++ services/image-service/src/controller.status.pas
@@ -17,8 +17,10 @@ type
   { TStatusController }
 
   TStatusController = class(TBaseController)
-  private
-
+  strict private
+    procedure InitStatusTable;
+  strict protected
+    procedure DoInitialize(); override;
   public
 
   end;
@@ -29,6 +31,31 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TStatusController }
+
+procedure TStatusController.InitStatusTable;
+var
+  LError: String;
+begin
+  //initialize our table if it doesn't exist
+  if not ExecuteSQL(
+    'CREATE TABLE IF NOT EXISTS status(' +
+    ' id Integer NOT NULL PRIMARY KEY AUTOINCREMENT,' +
+    ' registration_id integer,' +
+    ' status varchar(128) NOT NULL,' +
+    ' create_date datetime NOT NULL DEFAULT (datetime(''now'')),' +
+    ' FOREIGN KEY(registration_id) REFERENCES registration(id));',
+    LError
+  ) then
+    LogError(LError);
+end;
+
+procedure TStatusController.DoInitialize();
+begin
+  inherited DoInitialize();
+  InitStatusTable;
+end;
 
 initialization
   RegisterHTTPModule(GetControllerRoute('status'), TStatusController);
