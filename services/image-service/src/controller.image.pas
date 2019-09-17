562bbf03572ee446196d865ca48b5e7ab5c2ed52
mr-highball <35942565+mr-highball@users.noreply.github.com>
2019-09-16 20:25:57 -0400

    closes #4 - initialize the image table to hold commands

--- services/image-service/src/controller.image.pas
+++ services/image-service/src/controller.image.pas
@@ -18,7 +18,9 @@ type
 
   TImageController = class(TBaseController)
   private
-
+    procedure InitImageTable;
+  protected
+    procedure DoInitialize(); override;
   public
 
   end;
@@ -29,6 +31,31 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TImageController }
+
+procedure TImageController.InitImageTable;
+var
+  LError: String;
+begin
+  //initialize our table if it doesn't exist
+  if not ExecuteSQL(
+    'CREATE TABLE IF NOT EXISTS image(' +
+    ' id Integer NOT NULL PRIMARY KEY AUTOINCREMENT,' +
+    ' registration_id integer,' +
+    ' commands varchar(4000) NOT NULL,' +
+    ' create_date datetime NOT NULL DEFAULT (datetime(''now'')),' +
+    ' FOREIGN KEY(registration_id) REFERENCES registration(id));',
+    LError
+  ) then
+    LogError(LError);
+end;
+
+procedure TImageController.DoInitialize();
+begin
+  inherited DoInitialize();
+  InitImageTable;
+end;
 
 initialization
   RegisterHTTPModule(GetControllerRoute('image'), TImageController);
